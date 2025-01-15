c1043edff968ab639f40e1264198b43ec10c2f7c
Sthefany Silva <sthefanysilvaakn@gmail.com>
2025-01-15 02:26:42 -0300

    Add files via upload

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sthrodri <sthrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 02:21:58 by sthrodri          #+#    #+#             */
+/*   Updated: 2025/01/15 02:22:06 by sthrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char	*ft_read_line(int fd, char *line);
+char	*ft_get_next_line(char *line);
+char	*new_line(char *line);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+
+#endif
